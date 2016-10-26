88a4c0e726d60209edcd5a5e47df7de1c5ad9b46
Erik B. Andersen <erik.b.andersen@gmail.com>
2016-10-26 10:26:10 -0700

    Small bit of machinery that will be needed later.

--- rsp.cpp
+++ rsp.cpp
@@ -1,5 +1,11 @@
 #include "rsp_if.h"
 #include "rsp.h"
+#include <pthread.h>
+
+class RspData
+{
+    pthread_t rec_thread;
+};
 
 void rsp_init(int window_size)
 {
@@ -12,16 +18,19 @@ rsp_connection_t rsp_connect(const char *connection_name)
 
 int rsp_close(rsp_connection_t rsp)
 {
+    RspData * data = reinterpret_cast<RspData *>(rsp);
     return 0;
 }
 
 int rsp_write(rsp_connection_t rsp, void *buff, int size)
 {
+    RspData * data = reinterpret_cast<RspData *>(rsp);
     return 0;
 }
 
 int rsp_read(rsp_connection_t rsp, void *buff, int size)
 {
+    RspData * data = reinterpret_cast<RspData *>(rsp);
     return 0;
 }
 
