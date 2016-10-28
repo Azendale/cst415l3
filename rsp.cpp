9b05da412eb92c3752c5b083ab48cf1b98c629a4
Erik B. Andersen <erik.b.andersen@gmail.com>
2016-10-27 17:06:16 -0700

    Start the connect funciton. Test to see if libbsd is available.

--- rsp.cpp
+++ rsp.cpp
@@ -1,15 +1,24 @@
 #include "rsp_if.h"
 #include "rsp.h"
+#include "queue.h"
 #include <pthread.h>
 // memset, memcpy
 #include <string.h>
+#include <bsd/string.h>
+
+static int g_window  = 0;
 
 class RspData
 {
     // List for each piece of state what threads read, write it, and decide if lock is needed.
     pthread_t rec_thread;
+    // Set by rsp_connect, read by write and close, no lock needed
     uint16_t src_port;
     uint16_t dst_port;
+    uint64_t sequence;
+    uint64_t ack_sequence;
+    int farWindow;
+    
 };
 
 void * rsp_reader(void * args)
@@ -32,6 +41,22 @@ rsp_connection_t rsp_connect(const char *connection_name)
     memset(&response, 0, sizeof(response));
     
     // Send connection request
+    // fill out struct
+    //connection_name[RSP_MAX_CONNECTION_NAME_LEN + 1]
+    strncpy(request.connection_name, connection_name, RSP_MAX_CONNECTION_NAME_LEN + 1);
+    // src_port, dst_port already 0 from memset
+    // flags, 0 by default from memset
+    request.flags.flags.syn = 1;
+    // length -- 0 from memset, no payload on this syn, so already correct
+    // window
+    request.window = htons(g_window);
+    // sequence -- not doing acks yet, but seems like an OK place to start
+    // ack_sequence, not doing acks yet, nothing to ack on initial connect anyway
+    // buffer[RSP_MAX_SEND_SIZE] -- no payload, empty
+    
+    rsp_transmit(&request);
+
+    
     
     // Read (block) for SYNACK to connection
     
